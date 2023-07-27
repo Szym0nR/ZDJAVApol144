a07c1a42af469945fcc9f7e7d36f2426e2254fdf
Szym0nR <sarudowski1@gmail.com>
2023-07-27 20:33:59 +0200

    Introduction to classes.

---
+++ Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    private String model;
+    private int rocznik;
+    private String marka;
+
+    public void jazda(){
+        System.out.println("Teraz jade");
+    }
+}
---
+++ Pies.java
@@ -0,0 +1,12 @@
+public class Pies {
+    private String rasa;
+    private int wKlebie;
+    private String imie;
+    private int iloscNog;
+    private boolean czyOgon;
+
+    public Pies(String rasa, int wKlebie, String imie, int iloscNog, boolean czyOgon)
+    {
+
+    }
+}
